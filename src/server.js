d7e86f2240eef8f0965688a8838de561122296bd
Duvhier <duvier.tavera01@unicatolica.edu.co>
2025-11-16 18:00:29 -0500

    fix error server

--- src/server.js
+++ src/server.js
@@ -110,7 +110,6 @@ if (process.env.NODE_ENV === 'development') {
   });
 }
 
-// Rutas
 app.use('/api/auth', require('./routes/auth.routes'));
 app.use('/api/users', require('./routes/user.routes'));
 app.use('/api/products', require('./routes/product.routes'));
